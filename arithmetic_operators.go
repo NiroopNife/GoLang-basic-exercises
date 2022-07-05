91e728c67c9b72f27263da13667fcc027ffdb3b7
nifen <nife.niroopn@gmail.com>
2022-07-05 15:24:04 +0530

    Arithmetic Operators

---
+++ arithmetic_operators.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Addition :- 1 + 2 = ", 1+2)
+	fmt.Println("Substraction :- 5 - 3 = ", 5-3)
+	fmt.Println("Multiplication :- 4 * 6 = ", 4*6)
+	fmt.Println("Division :- 10 / 2 = ", 10/2)
+	fmt.Println("Remainder :- 3 / 2 = ", 3/2)
+
+}
